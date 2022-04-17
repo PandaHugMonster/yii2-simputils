66f6863b2b5e5e83c67574c1187a58d8e1621a01
Ivan Ponomarev <pi@spaf.dev>
2022-04-17 17:07:40 +0200

    new-features-2
    Seems like all the stuff is "monkey patching" the yii2 well!
    Release candidate!

--- run.php
+++ run.php
@@ -1,13 +1,13 @@
 #!/bin/env php
 <?php
 
+use spaf\simputils\attributes\DebugHide;
+use spaf\simputils\attributes\Extract;
 use spaf\simputils\attributes\Property;
 use spaf\simputils\PHP;
 use spaf\simputils\yii\base\BaseObject;
 use spaf\simputils\yii\components\InitConfig;
-use function spaf\simputils\basic\now;
 use function spaf\simputils\basic\pd;
-use function spaf\simputils\basic\pr;
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
@@ -25,19 +25,37 @@ $application = new spaf\simputils\yii\console\Application([
 /**
  * Class MyClass
  * @property-read string $me
+ * @property string $something
+ * @property ?\spaf\simputils\yii\components\DateTime $dt
  */
 class MyClass extends BaseObject {
+
+	#[Property]
+	public ?\spaf\simputils\yii\components\DateTime $_dt = null;
+
+	public $gogo;
 
 	#[Property('me')]
 	protected function tt(): string {
 		return 'TEST';
 	}
 
+	public function getSomething() {
+		return 'str';
+	}
+
+	public function setSomething($var) {
+		$this->gogo = $var;
+	}
 }
 
 
 $obj = new MyClass();
 
 
-pr($obj->me, PHP::type($sut_config), PHP::type(now()));
-pd(PHP::getInitConfig()->working_dir);
+//pr($obj->me, PHP::type($sut_config), PHP::type(now()));
+//pd(PHP::getInitConfig()->working_dir);
+//pd(isset($obj->something));
+$obj->something = '333';
+$obj->dt = '1234-05-05 01:02:03';
+pd($obj);
