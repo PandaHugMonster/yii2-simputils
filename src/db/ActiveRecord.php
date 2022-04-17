806e2a1c587b7f909710ff98d4410972784a135c
Ivan Ponomarev <pi@spaf.dev>
2022-04-17 17:34:58 +0200

    new-features-2
    Tiny fix 2

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -7,4 +7,9 @@ use spaf\simputils\traits\SimpleObjectTrait;
 class ActiveRecord extends \yii\db\ActiveRecord {
 	use SimpleObjectTrait;
 
+	public function toArray(array $fields = [], array $expand = [], $recursive = true) {
+		// FIX  Maybe should be improved
+		return $this->_toArray($recursive, false, []);
+	}
+
 }
