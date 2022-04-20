6aa580fe1bee2c08cd435eb0c7c78aca484a3c2e
Ivan Ponomarev <pi@spaf.dev>
2022-04-20 13:33:57 +0200

    new-features-2
    Tiny fix 4

--- src/db/BaseActiveRecord.php
+++ src/db/BaseActiveRecord.php
@@ -7,4 +7,18 @@ use spaf\simputils\traits\SimpleObjectTrait;
 abstract class BaseActiveRecord extends \yii\db\BaseActiveRecord {
 	use SimpleObjectTrait;
 
+	protected $_attributes = [];
+	/**
+	 * @var array|null old attribute values indexed by attribute names.
+	 * This is `null` if the record [[isNewRecord|is new]].
+	 */
+	protected $_oldAttributes;
+	/**
+	 * @var array related models indexed by the relation names
+	 */
+	protected $_related = [];
+	/**
+	 * @var array relation names indexed by their link attributes
+	 */
+	protected $_relationsDependencies = [];
 }
