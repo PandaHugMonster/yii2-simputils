6f343e6a74fc49e219292b628a7abce66c65fa12
Ivan Ponomarev <pi@spaf.dev>
2022-04-20 13:36:10 +0200

    new-features-2
    Tiny fix 4.5

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -7,6 +7,22 @@ use spaf\simputils\traits\SimpleObjectTrait;
 class ActiveRecord extends \yii\db\ActiveRecord {
 	use SimpleObjectTrait;
 
+
+	protected $_attributes = [];
+	/**
+	 * @var array|null old attribute values indexed by attribute names.
+	 * This is `null` if the record [[isNewRecord|is new]].
+	 */
+	protected $_oldAttributes;
+	/**
+	 * @var array related models indexed by the relation names
+	 */
+	protected $_related = [];
+	/**
+	 * @var array relation names indexed by their link attributes
+	 */
+	protected $_relationsDependencies = [];
+
 	public function toArray(array $fields = [], array $expand = [], $recursive = true) {
 		// FIX  Maybe should be improved
 		return $this->_toArray($recursive, false, []);
