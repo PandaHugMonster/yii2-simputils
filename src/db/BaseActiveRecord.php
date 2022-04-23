0589331b060eb9e5480eda60c19f928ab0311128
Ivan Ponomarev <pi@spaf.dev>
2022-04-23 13:52:12 +0200

    new-features-2
    Tiny fix 7

--- src/db/BaseActiveRecord.php
+++ src/db/BaseActiveRecord.php
@@ -2,8 +2,10 @@
 
 namespace spaf\simputils\yii\db;
 
+use ReflectionProperty;
 use spaf\simputils\traits\SimpleObjectTrait;
 use function array_flip;
+use function array_key_exists;
 use function is_null;
 
 abstract class BaseActiveRecord extends \yii\db\BaseActiveRecord {
@@ -16,6 +18,34 @@ abstract class BaseActiveRecord extends \yii\db\BaseActiveRecord {
 	 */
 	protected static function attributesPreNorming() {
 		return [];
+	}
+
+	public function __set($name, $value) {
+		$normalizer = static::attributesPreNorming()[$name] ?? null;
+		$prop_relations_dependencies = new ReflectionProperty(
+			'\yii\db\BaseActiveRecord', '_relationsDependencies'
+		);
+		$prop_relations_dependencies->setAccessible(true);
+		$prop_attributes = new ReflectionProperty(
+			'\yii\db\BaseActiveRecord', '_attributes'
+		);
+		$prop_attributes->setAccessible(true);
+
+		if ($this->hasAttribute($name)) {
+			if (
+				!empty($prop_relations_dependencies->getValue($this)[$name])
+				&& (!array_key_exists($name, $prop_attributes->getValue($this)) || $prop_attributes->getValue($this)[$name] !== $value)
+			) {
+				$this->resetDependentRelations($name);
+			}
+			$this->setAttribute($name, !is_null($value) && !is_null($normalizer)
+				?$normalizer::process($value)
+				:$value);
+		} else {
+			parent::__set($name, $value);
+		}
+		$prop_relations_dependencies->setAccessible(false);
+		$prop_attributes->setAccessible(false);
 	}
 
 	public static function populateRecord($record, $row) {
