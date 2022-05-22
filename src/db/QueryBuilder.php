6c4e55eefda7aec92fa72cfc51c7006aead3bc37
Ivan Ponomarev <pi@spaf.dev>
2022-05-22 17:03:46 +0200

    new-features-2
    Tiny fix 10

--- src/db/QueryBuilder.php
+++ src/db/QueryBuilder.php
@@ -2,9 +2,26 @@
 
 namespace spaf\simputils\yii\db;
 
+use spaf\simputils\models\DateTime;
 use spaf\simputils\traits\SimpleObjectTrait;
 
 class QueryBuilder extends \yii\db\QueryBuilder {
 	use SimpleObjectTrait;
+
+	public function update($table, $columns, $condition, &$params) {
+
+		foreach ($columns as $key => $val) {
+			if ($val instanceof DateTime) {
+				$columns[$key] = $val->for_system;
+			}
+		}
+		foreach ($params as $key => $val) {
+			if ($val instanceof DateTime) {
+				$params[$key] = $val->for_system;
+			}
+		}
+
+		return parent::update($table, $columns, $condition, $params);
+	}
 
 }
