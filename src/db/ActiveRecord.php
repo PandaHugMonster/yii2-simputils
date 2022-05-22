b2366f3baf738bad44371b2b972727c0c432e329
Ivan Ponomarev <pi@spaf.dev>
2022-05-22 22:21:49 +0200

    new-features-2
    Tiny fix 12

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -3,13 +3,58 @@
 namespace spaf\simputils\yii\db;
 
 use ReflectionProperty;
+use spaf\simputils\models\DateTime;
 use spaf\simputils\traits\SimpleObjectTrait;
+use function array_fill_keys;
 use function array_flip;
 use function array_key_exists;
+use function array_keys;
 use function is_null;
 
 class ActiveRecord extends \yii\db\ActiveRecord {
 	use SimpleObjectTrait;
+
+	protected function insertInternal($attributes = null) {
+		if (!$this->beforeSave(true)) {
+			return false;
+		}
+		$values = $this->getDirtyAttributes($attributes);
+		foreach ($values as $col => $val) {
+			if ($val instanceof DateTime) {
+				$values[$col] = $val->for_system;
+			}
+		}
+		if (($primaryKeys = static::getDb()->schema->insert(static::tableName(), $values)) === false) {
+			return false;
+		}
+		foreach ($primaryKeys as $name => $value) {
+			$id = static::getTableSchema()->columns[$name]->phpTypecast($value);
+			$this->setAttribute($name, $id);
+			$values[$name] = $id;
+		}
+
+		$changedAttributes = array_fill_keys(array_keys($values), null);
+		$this->setOldAttributes($values);
+		$this->afterSave(true, $changedAttributes);
+
+		return true;
+	}
+
+	public static function updateAll($attributes, $condition = '', $params = []) {
+		foreach ($attributes as $col => $val) {
+			if ($val instanceof DateTime) {
+				$attributes[$col] = $val->for_system;
+			}
+		}
+		foreach ($params as $col => $val) {
+			if ($val instanceof DateTime) {
+				$params[$col] = $val->for_system;
+			}
+		}
+		$command = static::getDb()->createCommand();
+		$command->update(static::tableName(), $attributes, $condition, $params);
+		return $command->execute();
+	}
 
 	/**
 	 * SimpUtils Normalizer/Validator functionality
